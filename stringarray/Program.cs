80e3dddc1bb1b85537e68d4efb89792529f379b6
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:28:50 +0530

    Initial commit

---
+++ stringarray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = new string[100];
+            Console.WriteLine("enter the car name");
+            for(int i=0;i<5;i++)
+            {
+                cars[i] = Console.ReadLine();
+            
+            }
+            for (int j = 0; j < cars.Length; j++)
+            {
+                Console.WriteLine("cars{0}", cars[j]);
+            }
+
+
+        }
+    }
+}
